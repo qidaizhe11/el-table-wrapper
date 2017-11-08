a6ce6c5625370e25c3f734ae0b2a1188721380f3
Panfeng <qidaizhe11@gmail.com>
2017-11-08 22:12:33 +0800

    Release 2.0.2

--- dist/el-table-wrapper.js
+++ dist/el-table-wrapper.js
@@ -1393,7 +1393,8 @@ var orderBy = function orderBy(array, sortKey, reverse, sortMethod) {
 
   // sort on a copy to avoid mutating original array
   return array.slice().sort(sortMethod ? function (a, b) {
-    return sortMethod(a, b) ? order : -order;
+    var result = sortMethod(a, b);
+    return result === 0 ? 0 : result > 0 ? order : -order;
   } : function (a, b) {
     if (sortKey !== '$key') {
       if (isObject(a) && '$value' in a) a = a.$value;
